b6ddec73e913768eb78582b9a19159e7d79ec0c5
Munyijwok James Alala <habib.munji@gmail.com>
2026-05-05 22:58:43 +0300

    Add Main class with a simple Hello World program

---
+++ MyFirst/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args){
+
+        //first programs
+
+        /*
+        mulitiline
+        comment
+
+         */
+        System.out.print("Hello World\n");
+        System.out.println("\nHello World");
+
+    }
+}
