f080a7eb6c150d0faff3b48c216ed6518cb202c5
Munyijwok James Alala <habib.munji@gmail.com>
2026-05-05 23:14:01 +0300

    Add personal variables and update output
    Rename generic variables to descriptive names (name, age) and add new primitives: salary (double), gender (char), and married (boolean). Update the System.out.println call to include the new fields in the printed message, improving readability and demonstrating additional variable types.

--- MyFirst/src/Variables.java
+++ MyFirst/src/Variables.java
@@ -1,7 +1,15 @@
 public class Variables {
     public static void main(String[] args){
-        String x = "Jane";
-        int y = 7;
-        System.out.println(x + y);
+        String name = "James";
+        int age = 25;
+
+        double salary = 70000;
+
+        char gender = 'M';
+
+        boolean married = false;
+
+
+        System.out.println("Hello" + " " + name + " " + "you are a " + age + " years old " + gender + "." + " You earn $" + salary + "You are " + married);
     }
 }
